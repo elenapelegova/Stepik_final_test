67b703c053a4f690a04d3f066bb9e33202832abd
Пелегова Елена Владимировна <epelegova@phoenixit.ru>
2023-05-17 21:24:05 +0500

    register new user

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_SUBMIT = (By.NAME, "registration_submit")
     
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -20,6 +24,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     
 class BasketPageLocators():
     BASKET_EMPTY = (By.CSS_SELECTOR, "#content_inner")
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 import pytest
+import time
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -22,6 +24,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_item_in_basket()
     
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -35,12 +38,13 @@ def test_guest_cant_see_success_message(browser):
     page.open()                      # открываем страницу
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is present, but should not be"
     
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.add_to_basket()
-    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), 'Sucsess message does not disappear' 
+    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), 'Success message does not disappear' 
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -53,3 +57,28 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open() 
     page.go_to_login_page()    
+    
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time()) + "Pass!!"
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+        
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.add_to_basket()
+        page.should_item_in_basket()
+        
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is present, but should not be"
+    
